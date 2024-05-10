cd783b964e0c3d75363b622a1720925f4008d6ee
yuyue98 <yuhonge123@163.com>
2024-05-11 01:59:40 +0800

    feat: 优化

--- src/main/java/com/cybozu/labs/langdetect/LangDetectException.java
+++ src/main/java/com/cybozu/labs/langdetect/LangDetectException.java
@@ -4,6 +4,7 @@ package com.cybozu.labs.langdetect;
  * @author Nakatani Shuyo
  */
 enum ErrorCode {
+    /** 报错信息 **/
     NoTextError, FormatError, FileLoadError, DuplicateLangError, NeedLoadProfileError, CantDetectError, CantOpenTrainData, TrainDataFormatError, InitParamError
 }
 
@@ -13,12 +14,12 @@ enum ErrorCode {
  */
 public class LangDetectException extends Exception {
     private static final long serialVersionUID = 1L;
-    private ErrorCode code;
+    private final ErrorCode code;
     
 
     /**
-     * @param code
-     * @param message
+     * @param code 错误码
+     * @param message 错误信息
      */
     public LangDetectException(ErrorCode code, String message) {
         super(message);
